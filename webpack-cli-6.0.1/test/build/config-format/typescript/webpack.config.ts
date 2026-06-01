94b52fe1f6bef68ae91a00a21b8785226ef1c841
YanHSama <1269987538@qq.com>
2026-06-01 16:59:29 +0800

    添加 webpack-cli 6.0.1

---
+++ webpack-cli-6.0.1/test/build/config-format/typescript/webpack.config.ts
@@ -0,0 +1,15 @@
+/** eslint-disable **/
+import * as path from "path";
+
+// cspell:ignore elopment
+const mode: string = "dev" + "elopment";
+const config = {
+  mode,
+  entry: "./main.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "foo.bundle.js",
+  },
+};
+
+export = config;
